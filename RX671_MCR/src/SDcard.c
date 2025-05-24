c669ddacf550c7127fddb62396ea94cd20992066
LAPTOP-2KJSDFGH\Natsuki <uc.aw.ac.ce@gmail.com>
2025-05-24 20:08:44 +0800

    SDカード関数を外部ファイルに移動

---
+++ RX671_MCR/src/SDcard.c
@@ -0,0 +1,70 @@
+//====================================//
+// インクルード
+//====================================//
+#include "SDcard.h"
+#include "r_longq_if.h"
+#include <stdint.h>
+//====================================//
+// グローバル変数の宣言
+//====================================//
+
+/////////////////////////////////////////////////////////////////////
+// モジュール名 SDcardinit
+// 処理概要  	SDカードの初期化
+// 引数     	なし
+// 戻り値    	なし
+/////////////////////////////////////////////////////////////////////
+sdc_sd_status_t SDcardinit(void)
+{
+	uint32_t work[200 / sizeof(uint32_t)];
+	sdc_sd_cfg_t sdc_sd_config;
+	uint32_t errorlog;
+
+	// ピン設定
+	R_SDHI_PinSetInit();
+
+	if(R_SDC_SD_Open(SD_CARD_NO, SDHI_CH0, work) == SDC_SD_SUCCESS)
+	{
+		// SDカードの初期化に成功
+
+		cnt0 = 0;
+		while (R_SDC_SD_GetCardDetection(SD_CARD_NO) != SDC_SD_SUCCESS)
+		{
+			if (cnt0 > 2000)
+			{
+				// SDカードが検出されない場合の処理
+				return R_SDC_SD_GetErrCode(SD_CARD_NO);; // SDカードが検出されない
+			}
+		}
+
+		// SDHI設定
+		sdc_sd_config.mode = SDC_SD_MODE_POLL | 
+							SDC_SD_MODE_SW | 
+							SDC_SD_MODE_MEM | 
+							SDC_SD_MODE_4BIT;
+		// SDカード電源設定
+		sdc_sd_config.voltage = SDC_SD_VOLT_3_3;
+
+		SDCARD_POWER = SDCARD_POWER_ON; // SDカードの電源ON
+		R_BSP_SoftwareDelay(1000,BSP_DELAY_MILLISECS); // SDカード電源が安定するまで待機
+
+		R_SDHI_PinSetTransfer();	// SDHIピンを転送モードに設定
+
+		if(R_SDC_SD_Initialize(SD_CARD_NO, &sdc_sd_config, SDC_SD_MODE_MEM) == SDC_SD_SUCCESS)
+		{
+			return SDC_SD_SUCCESS; // 初期化成功
+		}
+		else
+		{
+			// SDカードの初期化に失敗
+			return R_SDC_SD_GetErrCode(SD_CARD_NO);
+		}
+	}
+	else
+	{
+		// SDカードの初期化に失敗
+		return R_SDC_SD_GetErrCode(SD_CARD_NO);
+	}
+	
+	return SDC_SD_ERR; // 初期化失敗
+}
